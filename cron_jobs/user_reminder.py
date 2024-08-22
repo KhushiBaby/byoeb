708e4622b607e24203bcb9e97a9b5c52fbeecb9e
Bhuvan Sachdeva <Bhuvan>
2024-08-22 10:17:10 +0530

    fixed minor bug in prev reminders

--- cron_jobs/user_reminder.py
+++ cron_jobs/user_reminder.py
@@ -9,7 +9,7 @@ import sys
 
 sys.path.append(local_path + "/src")
 
-from database import UserDB, UserConvDB, AppLogger
+from database import UserDB, UserConvDB, BotConvDB, AppLogger
 from conversation_database import LoggingDatabase
 from messenger.whatsapp import WhatsappMessenger
 import os
@@ -21,7 +21,8 @@ template_name = "asha_reminder"
 
 user_db = UserDB(config)
 user_conv_db = UserConvDB(config)
-app_logger = LoggingDatabase(config)
+bot_conv_db = BotConvDB(config)
+app_logger = AppLogger()
 messenger = WhatsappMessenger(config, app_logger)
 
 print("Date: ", datetime.datetime.now())
@@ -37,13 +38,34 @@ for i, user_row in df.iterrows():
         print("User opted out: ", user_row["whatsapp_id"], user_row["opt out"])
         continue
 
+    # check last reminder message, if less than 7 days old, continue
+    last_reminder = bot_conv_db.find_most_recent_with_receiver_id(user_row["user_id"], message_type='asha_reminder')
+    print("Last reminder: ", last_reminder)
+    if last_reminder is not None and (datetime.datetime.now() - last_reminder["message_timestamp"]) < datetime.timedelta(days=7):
+        print("Not sending message to ", user_row["whatsapp_id"])
+        continue
+
+
     try:
         last_msg = user_conv_db.get_most_recent_message(user_row["user_id"])
         last_msg_type = last_msg.get("message_type", 'small-talk')
         day_delta = 2 if last_msg_type == 'onboarding_response' else 7
         if last_msg is None or ((datetime.datetime.now() - last_msg["message_timestamp"]) > datetime.timedelta(days=day_delta)):
             print("Sending message to ", user_row["whatsapp_id"])
-            messenger.send_template(user_row["whatsapp_id"], template_name, user_row["user_language"], None)
+            sent_msg_id = messenger.send_template(user_row["whatsapp_id"], template_name, user_row["user_language"], None)
+            bot_conv_db.insert_row(
+                receiver_id=user_row['user_id'],
+                message_type=f'asha_reminder',
+                message_id=sent_msg_id,
+                audio_message_id=None,
+                message_source_lang=None,
+                message_language=user_row["user_language"],
+                message_english=None,
+                reply_id=None,
+                citations=None,
+                message_timestamp=datetime.datetime.now(),
+                transaction_message_id=None,
+            )
         else:
             print("Not sending message to ", user_row["whatsapp_id"])
     except Exception as e:
